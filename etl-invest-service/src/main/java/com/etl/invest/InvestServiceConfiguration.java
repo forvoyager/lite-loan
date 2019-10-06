1490f940aadee0eaeac5737ad4548ef12c92e5bc
forvoyager@outlook.com <forvoyager@outlook.com>
2019-10-07 07:53:31 +0800

    hessian协议

--- etl-invest-service/src/main/java/com/etl/invest/InvestServiceConfiguration.java
+++ etl-invest-service/src/main/java/com/etl/invest/InvestServiceConfiguration.java
@@ -1,6 +1,9 @@
 package com.etl.invest;
 
+import org.apache.dubbo.remoting.http.servlet.DispatcherServlet;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,4 +14,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan(basePackageClasses = {InvestServiceConfiguration.class})
 public class InvestServiceConfiguration {
+
+  /**
+   * 使用hessian协议暴露服务相关配置：
+   * 1、启用此servlet
+   * 2、调整server.context-path=/
+   * 3、服务协议配置如下：
+   *  service.protocol.name=hessian
+   *  service.protocol.server=servlet
+   *  service.protocol.port=${server.port}
+   * @return
+   */
+  @Bean
+  public ServletRegistrationBean hessianServle() {
+    ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new DispatcherServlet(), "/*");
+    servletRegistrationBean.setName("Hessian_Dispatcher_Servlet");
+    servletRegistrationBean.setLoadOnStartup(1);
+    return servletRegistrationBean;
+  }
+
 }
