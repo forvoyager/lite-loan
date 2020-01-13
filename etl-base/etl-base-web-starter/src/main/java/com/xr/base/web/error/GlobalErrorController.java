7f4fea9be226bc745fec747ff3079aaced5bab00
forvoyager@outlook.com <forvoyager@outlook.com>
2020-01-13 15:26:57 +0800

    全局异常调整

--- etl-base/etl-base-web-starter/src/main/java/com/xr/base/web/error/GlobalErrorController.java
+++ etl-base/etl-base-web-starter/src/main/java/com/xr/base/web/error/GlobalErrorController.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolve
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,6 +31,13 @@ public class GlobalErrorController extends BasicErrorController {
   public GlobalErrorController(ErrorAttributes errorAttributes, ErrorProperties errorProperties, List<ErrorViewResolver> errorViewResolvers) {
     super(errorAttributes, errorProperties, errorViewResolvers);
   }
+
+//  @RequestMapping
+//  @ResponseBody
+//  public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+//    Map<String, Object> body = getErrorAttributes(request, isIncludeStackTrace(request, MediaType.ALL));
+//    return new ResponseEntity<>(body, HttpStatus.OK);
+//  }
 
   @Override
   public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
